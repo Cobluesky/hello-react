d09b713fce06fbf6c5ca7834a56cb69b26bdd7a8
Bluesky <37494052+Cobluesky@users.noreply.github.com>
2022-03-21 21:56:39 +0900

    2022-03-21 - 2
    useState 다중 사용

--- src/Say.js
+++ src/Say.js
@@ -4,10 +4,25 @@ const Say = () => {
     const [message, setMessage] = useState('');
     const onClickEnter = () => setMessage('HI!');
     const onClickLeave = () => setMessage('BYE!');
+
+    const [color, setColor] = useState('black');
+
     return (
         <div>
             <button onClick={onClickEnter}>Enter</button>
             <button onClick={onClickLeave}>Leave</button>
+            <h1 style={{ color }}>{message}</h1>
+            <button style={{ color: 'red' }} onClick={() => setColor('red')}>
+                Red
+            </button>
+
+            <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+                Blue
+            </button>
+
+            <button style={{ color: 'green' }} onClick={() => setColor('green')}>
+                Green
+            </button>
             <h1>{message}</h1>
         </div>
     );
