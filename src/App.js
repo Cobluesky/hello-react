a0db05bfbccb90acbbced4762a8910191ef1805e
Bluesky <37494052+Cobluesky@users.noreply.github.com>
2022-04-01 23:59:20 +0900

    20220401
    react practice

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import LifeCycleSample from './LifeCycleSample';
 
 import { Component } from 'react';
 
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+  state =
+    {
+      color: '#000000'
+    }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
       <div>
-        <LifeCycleSample />
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
       </div>
     );
   }
